64bf76d485c18e7082136408e2319e41e733690e
sandiarbaa <sandiarbaa@gmail.com>
2025-09-16 10:25:55 +0700

    setup react vite ts + shadcn ui

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+// import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center p-5">
+      <Card className="w-80">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
